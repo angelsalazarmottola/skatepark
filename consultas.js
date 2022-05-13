1c147e2cec6d46e4c31b40de561ce994a8976738
Angel Salazar <angelsalazarmottola@gmail.com>
2022-05-12 20:14:40 -0400

    Se cambió el nombre de la base de datos

--- consultas.js
+++ consultas.js
@@ -3,7 +3,7 @@ const {Pool} = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'programadores',
+    database: 'skatepark_2',
     password: '1309',
     port: 5432,
 });
